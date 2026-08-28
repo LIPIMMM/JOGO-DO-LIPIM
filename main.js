a3753859248c125960c9ffdb325f5e8dfb287ca7
LIPIMMM <felipe.eliseu@escola.pr.gov.br>
2026-08-28 12:33:38 +0000

    Atualiza lógica de início do jogo

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 const Mario=document.querySelector('.Mario')
 const pipe=document.querySelector('pipe')
-const startButton=document.querySelector('starButton')
+const sta//rtButton=document.querySelector('starButton')
 const restartButton=document.querySelector('restartButton')
 const gameOver=document.querySelector('gameOver')
 
@@ -16,7 +16,39 @@ const audioMario=new Audio ('sound/MARIO_soung_audio_theme.mp3')
 
 let gemeInterval=null
 
-// funçẽs para iniciar o jogo
+ //funçẽs para iniciar o jogo
 
 const startGame=()=>{}
 pipe.classList.add('pipe-animation')
+
+// oculta o botão de "iniciar" para evitar cliques acidentais
+
+startBotton.style.display = 'none'
+
+// oculta a tela de gameOver (do jogo anterior)
+
+gameOver.style.display = 'none'
+
+// iniciar a reprodução da musica tema
+
+audioStart.correntTime = 0
+
+// iniciar a reprodução da musica tema
+
+audioStart.play()
+
+// verificar se o intervalo do jogo já não está rodando
+
+if(!gameInterval){
+    // criar um intervalo que executa a função gameloop a cada 10 milisegundos
+    // isso atualiza a detecção de colisão continuamente
+
+gameInterval = s
+
+
+
+
+
+
+}
+
